b6810fdc5d0708dea2fe390fccdb24b8228d35de
huydeptrai <ldmh2042002@gmail.com>
2021-10-07 12:58:32 +0700

    first upload

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// render: hàm của react giúp tạo UI
+// có 2 tham số: nội dung UI, thẻ sẽ được thêm nội dung
+// <App/> thẻ được dev tạo ra, coi như là các component
+
+ReactDOM.render(
+    //<React.StrictMode>
+    <
+    App / > ,
+    //</React.StrictMode>,
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
